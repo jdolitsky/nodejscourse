84ef73b34bd6bc0b85c25ef6d137cfa65ddce93d
Josh Dolitsky <user@dhcp67.corp.mediatemple.net>
2013-07-23 13:36:11 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// required modules
+var express = require('express');
+
+// initialize our app
+var app = express();
+
+// app configuation
+app.set('views', __dirname+'/views');
+app.use(express.static(__dirname+'/public'));
+app.use(express.logger('dev'));
+
+// port that server will listen on
+var port = 3000;
+
+// start listening...
+app.listen(port);
+console.log('Express server listening on port '+port);
+
+// root route (response for http://localhost:3000/)
+app.get('/', function (req, res) {
+
+  res.render('homepage.ejs', {message: "Hello world!"} );
+
+});
