51debfdc554b9dc9438bab45df4e08dd6c414c0b
jdolitsky <jdolitsky@mediatemple.net>
2013-08-23 11:27:03 -0700

    styling

--- server.js
+++ server.js
@@ -4,17 +4,15 @@ var mongoose = require('mongoose');
 var engine = require('ejs-locals');
 
 // connect to MongoDB
-var db = 'test3';
+var db = 'coloft';
 mongoose.connect('mongodb://localhost/'+db);
 
 // initialize our app
 var app = express();
 
-app.engine('ejs', engine);
-
 // app configuation
+app.engine('ejs', engine);
 app.set('views', __dirname+'/views');
-
 app.use(express.static(__dirname+'/public'));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -28,45 +26,49 @@ var port = 3000;
 app.listen(port);
 console.log('Express server listening on port '+port);
 
-// create database schema for a user model
-var userSchema = mongoose.Schema({
+
+// create user model 
+var User = mongoose.model('User', {
 	username: String,
 	password: String,
 	image: String,
 	bio: String,
 });
 
-var statusSchema = mongoose.Schema({
-	status: String,
-	comments: Array,
+// create status model
+var Status = mongoose.model('Status', {
+	body: String,
 	time: Number,
 	username: String,
-	image: String
+	image: String,
+	comments: Array,
+	likes: Array
 });
 
-// create user model using schema
-var User = mongoose.model('User', userSchema);
-var Status = mongoose.model('Status', statusSchema);
-// root route (response for http://localhost:3000/)
 app.get('/', function (req, res) {
-	console.log(req.session.user);
+
 	if (req.session.user){
+
 		Status.find({},function (err, statuses){
 			res.render('homepage.ejs', {user: req.session.user, statuses: statuses});
 		});
 
 	} else {
+
 		res.render('welcome.ejs');
 	}
 
 });
 
 app.get('/logout', function (req, res) {
+
 	delete req.session.user;
 	res.redirect('/login');
+
 });
 
 app.get('/login', function (req, res) {
+
 	var error1 = null;
 	var error2 = null;
 
@@ -79,10 +81,11 @@ app.get('/login', function (req, res) {
 	}
 
 	res.render('login.ejs', {error1: error1, error2: error2});
+
 });
 
 app.post('/login', function (req, res) {
-	var username = req.body.username;
+	var username = req.body.username.toLowerCase();
 	var password = req.body.password;
 
 	var query = {username: username, password: password};
@@ -101,12 +104,15 @@ app.post('/login', function (req, res) {
 
 
 app.post('/signup', function (req, res){
-	var username = req.body.username;
+
+	var username = req.body.username.toLowerCase();
 	var password = req.body.password;
 	var confirm = req.body.confirm;
+
 	if(password != confirm){
 		res.redirect('/login?error1=1');
 	}
+
 	else{
 		var query = {username: username};
 		console.log(query);
@@ -115,19 +121,23 @@ app.post('/signup', function (req, res){
 			if (user) {
 				res.redirect('/login?error1=1');
 			} else {
-					var userData = { 
-						username: username,
-						password: password,
-						image: 'http://leadersinheels.com/wp-content/uploads/facebook-default.jpg', //default image
-						bio: 'Welcome to NodeBook! Edit your Profile here',
-						hidden: false,
-						wall: []
-						};
-					var newUser = new User(userData).save(function (err){
-						req.session.user = userData;
-						console.log('New user: '+newUser+' has been created!');
-						res.redirect('/users/'+username);
-					});
+
+				var userData = { 
+					username: username,
+					password: password,
+					image: 'http://leadersinheels.com/wp-content/uploads/facebook-default.jpg', //default image
+					bio: 'Im new to NodeBook!',
+					hidden: false,
+					wall: []
+				};
+
+				var newUser = new User(userData).save(function (err){
+
+					req.session.user = userData;
+					console.log('New user: '+newUser+' has been created!');
+					res.redirect('/users/'+username);
+					
+				});
 			}
 		});
 	}
@@ -135,10 +145,13 @@ app.post('/signup', function (req, res){
 
 // user profile
 app.get('/users/:username', function (req, res) {
-	var username = req.params.username;
+
+	var username = req.params.username.toLowerCase();
 	var query = {username: username};
 	var currentUser = req.session.user;
+
 	User.findOne(query, function (err, user) {
+
 		if (err || !user) {
 			res.send('No user found by id '+username);
 		} else {
@@ -149,29 +162,11 @@ app.get('/users/:username', function (req, res) {
 					currentUser: currentUser
 				});	
 			});
-			
 		}
+
 	});
 });
 
-app.post('/statuses', function (req, res) {
-	
-	var status = req.body.status;
-	var username = req.session.user.username;
-	var pic = req.session.user.image;
-
-	var newStatus = new Status({ 
-		status: status,
-		comments: [],
-		time: new Date().getTime(),
-		username: username,
-		image: pic	
-	}).save(function (err){
-		res.redirect('/users/'+username);
-	});
-});
-
-// update bio
 app.post('/bio', function (req, res) {
 	
 	var username = req.session.user.username;
@@ -180,9 +175,13 @@ app.post('/bio', function (req, res) {
 	var newBio = req.body.bio;
 
 	User.findOne(query, function (err, user) {
+
 		if (err || !user) {
+
 			res.send('No user found by name '+username);
+
 		} else {
+
 			user.bio = newBio;
 			user.save(function(err) {
 			    if (err) {
@@ -190,7 +189,29 @@ app.post('/bio', function (req, res) {
 			    } else {
 			    	res.redirect('/users/'+username);
 			    }
-			  });
+			});
+
 		}
 	});
+});
+
+app.post('/statuses', function (req, res) {
+
+	var status = req.body.status;
+	var username = req.session.user.username;
+	var pic = req.session.user.image;
+
+	var newStatus = new Status({ 
+		body: status,
+		time: new Date().getTime(),
+		username: username,
+		image: pic,
+		comments: [],
+		likes: []
+	}).save(function (err){
+
+		res.redirect('/users/'+username);
+
+	});
+
 });
